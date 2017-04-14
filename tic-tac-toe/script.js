45f5c929f654215d40c99064fb1a399342f2caf8
Yiyio <indaghetto@gmail.com>
2017-04-14 21:40:15 +0200

    sketching game loop

---
+++ tic-tac-toe/script.js
@@ -0,0 +1,23 @@
+//Determine who goes first with popup: variable turn = human/computer
+
+//Clear the board
+var win = false;
+var tie = false;
+var turn = 'human';
+
+while (win === false && tie === false) {
+  console.log('asdf')
+  if (turn === 'human'){
+    //wait for input
+    //update board
+    //switch turn
+    //if three in a row, win
+    break;
+  } else if (turn === 'computer') {
+    //play some AI, white boy
+    //update board
+    //switch turn
+    //if three in a row, win
+  }
+  break;
+}
