f2573c9ad12fbccd1588b59111f8d7e3ebf9aef7
Yiyio <indaghetto@gmail.com>
2017-04-16 00:19:29 +0200

    added player moves

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,23 +1,46 @@
-//Determine who goes first with popup: variable turn = human/computer
+var moves = 0;
 
-//Clear the board
-var win = false;
-var tie = false;
-var turn = 'human';
 
-while (win === false && tie === false) {
-  console.log('asdf')
-  if (turn === 'human'){
-    //wait for input
-    //update board
-    //switch turn
-    //if three in a row, win
-    break;
-  } else if (turn === 'computer') {
-    //play some AI, white boy
-    //update board
-    //switch turn
-    //if three in a row, win
-  }
-  break;
+
+var getRandomInt = function(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
 }
+
+var humanTurn = function(){
+  var numbers = document.getElementsByClassName('square');
+
+  for (var i = 0; i < numbers.length + 1; i++) {
+  numbers[i].addEventListener('click', function() {
+    if (moves % 2 === 0){
+  document.getElementById(this.getAttribute('id')).innerHTML = 'X';
+    moves += 1;
+    };
+  });
+};
+};
+
+
+var computerTurn = function() {
+    //play some naive AI
+    var AIStrategy = getRandomInt(1,10);
+    console.log(AIStrategy)
+    while (document.getElementById(AIStrategy).innerText !== ''){
+      //fix when everything is occuppied
+      console.log(AIStrategy)
+      AIStrategy = getRandomInt(1,10);
+      console.log(AIStrategy)
+    }
+    document.getElementById(AIStrategy).innerHTML = 'O';
+    moves += 1;
+  };
+
+
+var checkWinningConditions = function(){
+
+
+}
+
+humanTurn();
+computerTurn();
