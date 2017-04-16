cf7fa7078e3c978abc490f316e6af011250d2e2b
Yiyio <indaghetto@gmail.com>
2017-04-16 17:28:05 +0200

    trying to avoid hard coding winning conditions

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,12 +1,40 @@
 var moves = 0;
+var board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+var wins = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+ ];
 
-
-
+///Create some helper functions
 var getRandomInt = function(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
+var createGroupedArray = function(arr, chunkSize) {
+    var groups = [], i;
+    for (i = 0; i < arr.length; i += chunkSize) {
+        groups.push(arr.slice(i, i + chunkSize));
+    }
+    return groups;
+}
+
+function findOccurrences(arr, val) {
+    var i, j,
+        count = 0;
+    for (i = 0, j = arr.length; i < j; i++) {
+        (arr[i] === val) && count++;
+    }
+    return count;
+}
+
 
 var humanTurn = function(){
   var numbers = document.getElementsByClassName('square');
@@ -16,6 +44,7 @@ var humanTurn = function(){
     if (moves % 2 === 0){
   document.getElementById(this.getAttribute('id')).innerHTML = 'X';
     moves += 1;
+      board[this.getAttribute('id')] = 'X';
     };
   });
 };
@@ -33,14 +62,35 @@ var computerTurn = function() {
       console.log(AIStrategy)
     }
     document.getElementById(AIStrategy).innerHTML = 'O';
+   board[AIStrategy] = 'O';
     moves += 1;
   };
 
 
-var checkWinningConditions = function(){
+ var checkWinner = function(board){
+  for(var i=0; i<this.wins.length; i++){
+    var a, b, c;
+    console.log(i)
+    //these varaibles become what the board is holding 'x', 'o', or 0
+    a = board[wins[i][0]];
+    b = board[wins[i][1]];
+    c = board[wins[i][2]];
 
-
+    if(a == b && a == c && a !== 0){
+      return a;
+    }
+  }
+  return false;
 }
+
+
+
+  if(moves === 9){
+    console.log("It's a trap! I mean...tie")
+  }
+
+
 
 humanTurn();
 computerTurn();
+checkWinner(board);
